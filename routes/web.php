148442848ad02a6154156e7ae86665105c820851
willywin99 <willywinsianturi@gmail.com>
2021-02-16 01:46:23 +0700

    Manajemen Produk - Fitur List/Tambah/Edit/Hapus Produk

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::group(
     function(){
         Route::get('dashboard', 'DashboardController@index');
         Route::resource('categories', 'CategoryController');
+        Route::resource('products', 'ProductController');
     }
 );
 
